f017bd12add804bdaeb2c1b839f9bfd2a76b19b7
Benny <theullrich@gmail.com>
2016-04-17 19:10:39 -0500

    initial commit

---
+++ assets/js/plugins/promo.js
@@ -0,0 +1,4 @@
+$(function() {
+	$(".fullscreen-static-image").backstretch(["http://intermosoft.com/wp-content/themes/BootstrapWP-child/assets/img/newlogofull.jpg"],{duration: 5000, fade:1500});
+});
+
---
+++ contact-process.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if( isset($_POST['name']) )
+{
+	$to = 'info@ibogaineclinic.com'; // Replace with your email	
+	$subject = 'Message from website'; // Replace with your $subject
+	$headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];	
+	
+	$message = 'Name: ' . $_POST['name'] . "\n" .
+	           'E-mail: ' . $_POST['email'] . "\n" .
+               'Phone: ' . $_POST['phone'] . "\n" .  
+	           'Message: ' . $_POST['message'];
+	
+	mail($to, $subject, $message, $headers);	
+	if( $_POST['copy'] == 'on' )
+	{
+		mail($_POST['email'], $subject, $message, $headers);
+	}
+}
+?>
